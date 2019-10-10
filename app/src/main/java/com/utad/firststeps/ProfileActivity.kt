1b6bab34e316110f92f2628bdd00ceceb6fb6fd9
DiegoValberdi <diegovalberdi.utad@gmail.com>
2019-10-10 14:52:32 +0200

    Made relation between elements on activity_profile and ProfileActivity TODO: display the info passed via Intent, assert scroll is functional

--- app/src/main/java/com/utad/firststeps/ProfileActivity.kt
+++ app/src/main/java/com/utad/firststeps/ProfileActivity.kt
@@ -2,11 +2,32 @@ package com.utad.firststeps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var imgProfile: ImageView
+    private lateinit var txtNameProfile: TextView
+    private lateinit var txtBirthDate: TextView
+    private lateinit var txtCity: TextView
+    private lateinit var txtDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        imgProfile = findViewById(R.id.imgProfile)
+        txtNameProfile = findViewById(R.id.txtNameProfile)
+        txtBirthDate = findViewById(R.id.txtBirthDate)
+        txtCity = findViewById(R.id.txtCity)
+        txtDescription = findViewById(R.id.txtDescription)
+
+        val image = intent.extras?.get("image")
+        val name = intent.extras?.get("name")
+        val birthDate = intent.extras?.get("birth_date")
+        val city = intent.extras?.get("city")
+        val description = intent.extras?.get("description")
+
+
     }
 }
