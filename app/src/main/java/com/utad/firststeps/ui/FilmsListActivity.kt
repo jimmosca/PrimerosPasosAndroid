484002472cecfd5c6a199da7dadafba1b1650fb6
Jaime Casado <jaime.casado.a@gmail.com>
2019-10-20 17:31:29 +0200

    Created and finalized MovieAdapter, following the documentation guide

--- app/src/main/java/com/utad/firststeps/ui/FilmsListActivity.kt
+++ app/src/main/java/com/utad/firststeps/ui/FilmsListActivity.kt
@@ -2,12 +2,17 @@ package com.utad.firststeps.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.utad.firststeps.R
+import kotlinx.android.synthetic.main.activity_films_list.*;
 
 class FilmsListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films_list)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter =
     }
 }
